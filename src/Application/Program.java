3b967b390005fb1cc98f257c0a5dd3c21cca16a6
Moises Santos <moisesbarbosa23@gmail.com>
2022-07-01 15:08:34 -0300

    add: insert values

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,32 +1,51 @@
 package Application;
 import DB.DB;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("SELECT * FROM department");
 
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+//            st = conn.prepareStatement(
+//                    "INSERT INTO seller" +
+//                            "(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
+//                            "VALUES " +
+//                            "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+//            st.setString(1, "Carl Purple");
+//            st.setString(2, "carlpurple@gmail.com");
+//            st.setDate(3, new java.sql.Date(sdf.parse("01/09/1999").getTime()));
+//            st.setDouble(4, 3000);
+//            st.setInt(5, 4);
+
+            st = conn.prepareStatement("INSERT INTO department (Name) values ('D1'), ('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id );
+                }
+            } else {
+                System.out.println("No rows affected!");
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            DB.closeResultSet(rs);
             DB.closeStatement(st);
             DB.closeConnection();
         }
-    }
+        }
 }
--- src/DB/DB.java
+++ src/DB/DB.java
@@ -14,7 +14,7 @@ public class DB {
         if (conn == null) {
             try {
                 Properties props = loadProperties();
-                String url = props.getProperty("url");
+                String url = props.getProperty("dburl");
                 conn = DriverManager.getConnection(url, props);
             } catch (SQLException e) {
                 throw new DbException(e.getMessage());
